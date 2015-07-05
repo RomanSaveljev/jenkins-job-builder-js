b36ba0b2d1d07b1f872ff3dca91a963cd355b02e
Roman Saveljev <roman.saveljev@haltian.com>
2015-07-05 21:41:08 +0300

    Implement delivery-pipeline wrapper and tests

---
+++ test/test_delivery-pipeline.js
@@ -0,0 +1,10 @@
+var should = require('should');
+var DeliveryPipeline = require('../lib/wrappers/delivery-pipeline.js');
+var testlib = require('./testlib.js');
+
+describe('DeliveryPipeline', function() {
+  describe('constructor', testlib.describeObjectProxyConstructor(DeliveryPipeline));
+  describe('object', testlib.describeObjectProxyUppableAndable(DeliveryPipeline));
+  describe('properties', testlib.describeObjectProxyProperty(DeliveryPipeline, 'versionTemplate', 'version-template'));
+  describe('properties', testlib.describeObjectProxyProperty(DeliveryPipeline, 'setDisplayName', 'set-display-name'));
+});
