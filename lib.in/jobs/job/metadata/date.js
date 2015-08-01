19cdabd03fbabcac2011fb5a6bcaeb69934f16d7
Roman Saveljev <roman.saveljev@haltian.com>
2015-08-01 17:23:40 +0300

    Convert metadata

--- lib.in/jobs/job/metadata/date.js
+++ lib.in/jobs/job/metadata/date.js
@@ -2,14 +2,8 @@
 
 var util = require('../../../misc/util.js');
 
-var DateObject = function(upper, obj) {
-  this.upper = upper;
-  this.obj = obj;
-};
-util.makeUppable(DateObject.prototype, 'upper');
-DateObject.prototype.name = util.generatePrimitiveAccessor('obj', 'name');
-DateObject.prototype.time = util.generatePrimitiveAccessor('obj', 'time');
-DateObject.prototype.timezone = util.generatePrimitiveAccessor('obj', 'timezone');
-DateObject.prototype.exposeToEnv = util.generatePrimitiveAccessor('obj', 'expose-to-env');
-
-module.exports = DateObject;
+uppableObjectProxy(DateObject)
+primitive(util, DateObject, 'name')
+primitive(util, DateObject, 'time')
+primitive(util, DateObject, 'timezone')
+primitive(util, DateObject, 'expose-to-env')
--- lib.in/jobs/job/metadata/primitive.js
+++ lib.in/jobs/job/metadata/primitive.js
@@ -2,13 +2,7 @@
 
 var util = require('../../../misc/util.js');
 
-var Primitive = function(upper, obj) {
-  this.upper = upper;
-  this.obj = obj;
-};
-util.makeUppable(Primitive.prototype, 'upper');
-Primitive.prototype.name = util.generatePrimitiveAccessor('obj', 'name');
-Primitive.prototype.value = util.generatePrimitiveAccessor('obj', 'value');
-Primitive.prototype.exposeToEnv = util.generatePrimitiveAccessor('obj', 'expose-to-env');
-
-module.exports = Primitive;
+uppableObjectProxy(Primitive)
+primitive(util, Primitive, 'name')
+primitive(util, Primitive, 'value')
+primitive(util, Primitive, 'expose-to-env')
