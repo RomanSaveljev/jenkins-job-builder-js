47136f9bbf23ad29f517de55272e194837567cb0
Roman Saveljev <roman.saveljev@haltian.com>
2015-08-01 17:24:25 +0300

    Convert notifications

--- lib.in/jobs/job/notifications/http.js
+++ lib.in/jobs/job/notifications/http.js
@@ -2,15 +2,9 @@
 
 var util = require('../../../misc/util.js');
 
-var Http = function(upper, obj) {
-  this.upper = upper;
-  this.obj = obj;
-};
-util.makeUppable(Http.prototype, 'upper');
-Http.prototype.format = util.generatePrimitiveAccessor('obj', 'format');
-Http.prototype.event = util.generatePrimitiveAccessor('obj', 'event');
-Http.prototype.url = util.generatePrimitiveAccessor('obj', 'url');
-Http.prototype.timeout = util.generatePrimitiveAccessor('obj', 'timeout');
-Http.prototype.log = util.generatePrimitiveAccessor('obj', 'log');
-
-module.exports = Http;
+uppableObjectProxy(Http)
+primitive(util, Http, 'format')
+primitive(util, Http, 'event')
+primitive(util, Http, 'url')
+primitive(util, Http, 'timeout')
+primitive(util, Http, 'log')
