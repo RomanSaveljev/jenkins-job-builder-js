c4afec77eaf0966903f09735ab37a7964a150072
Roman Saveljev <roman.saveljev@haltian.com>
2015-08-01 17:20:20 +0300

    Convert execution-strategy

--- lib.in/jobs/job/execution-strategy/touchstone.js
+++ lib.in/jobs/job/execution-strategy/touchstone.js
@@ -2,12 +2,6 @@
 
 var util = require('../../../misc/util.js');
 
-var Touchstone = function(upper, obj) {
-  this.upper = upper;
-  this.obj = obj;
-};
-util.makeUppable(Touchstone.prototype, 'upper');
-Touchstone.prototype.expr = util.generatePrimitiveAccessor('obj', 'expr');
-Touchstone.prototype.result = util.generatePrimitiveAccessor('obj', 'result');
-
-module.exports = Touchstone;
+uppableObjectProxy(Touchstone)
+primitive(util, Touchstone, 'expr')
+primitive(util, Touchstone, 'result')
