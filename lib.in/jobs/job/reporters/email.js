969e9e7534037f664dced5d8ed1047fc7064b9d8
Roman Saveljev <roman.saveljev@haltian.com>
2015-08-01 20:24:48 +0300

    Convert reporters

--- lib.in/jobs/job/reporters/email.js
+++ lib.in/jobs/job/reporters/email.js
@@ -2,13 +2,7 @@
 
 var util = require('../../../misc/util.js');
 
-var Email = function(upper, obj) {
-  this.upper = upper;
-  this.obj = obj;
-};
-util.makeUppable(Email.prototype, 'upper');
-Email.prototype.recipients = util.generatePrimitiveAccessor('obj', 'recipients');
-Email.prototype.notifyEveryUnstableBuild = util.generatePrimitiveAccessor('obj', 'notify-every-unstable-build');
-Email.prototype.sendToIndividuals = util.generatePrimitiveAccessor('obj', 'send-to-individuals');
-
-module.exports = Email;
+uppableObjectProxy(Email)
+primitive(util, Email, 'recipients')
+primitive(util, Email, 'notify-every-unstable-build')
+primitive(util, Email, 'send-to-individuals')
