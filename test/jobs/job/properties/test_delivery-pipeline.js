de09480930f75aba746020bd57458abc9b3eed45
Roman Saveljev <roman.saveljev@haltian.com>
2015-08-02 15:23:46 +0300

    Test delivery-pipeline property

---
+++ test/jobs/job/properties/test_delivery-pipeline.js
@@ -0,0 +1,10 @@
+var should = require('should');
+var DeliveryPipeline = require('../../../../lib/jobs/job/properties/delivery-pipeline.js');
+var testlib = require('../../../testlib.js');
+
+describe('DeliveryPipeline', function() {
+  describe('constructor', testlib.describeProxyConstructor(DeliveryPipeline));
+  describe('object', testlib.describeProxyUppableAndable(DeliveryPipeline));
+  describe('properties', testlib.describeProxyPrimitiveProperty(DeliveryPipeline, 'stage'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(DeliveryPipeline, 'task'));
+});
