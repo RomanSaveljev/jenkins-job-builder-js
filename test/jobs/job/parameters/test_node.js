a5e9040bce9d91a84a8cafb7aa3690ff6158a162
Roman Saveljev <roman.saveljev@haltian.com>
2015-08-02 15:11:18 +0300

    Test node parameter

---
+++ test/jobs/job/parameters/test_node.js
@@ -0,0 +1,14 @@
+var should = require('should');
+var Node = require('../../../../lib/jobs/job/parameters/node.js');
+var testlib = require('../../../testlib.js');
+
+describe('Node', function() {
+  describe('constructor', testlib.describeProxyConstructor(Node));
+  describe('object', testlib.describeProxyUppableAndable(Node));
+  describe('properties', testlib.describeProxyPrimitiveProperty(Node, 'name'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(Node, 'description'));
+  describe('properties', testlib.describeProxyPrimitiveArrayProperty(Node, 'default-nodes'));
+  describe('properties', testlib.describeProxyPrimitiveArrayProperty(Node, 'allowed-slaves'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(Node, 'ignore-offline-nodes'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(Node, 'allowed-multiselect'));
+});
