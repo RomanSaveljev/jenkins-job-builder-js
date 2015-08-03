6bf6b036c819a817615881a8c6819878ac945f8a
Roman Saveljev <roman.saveljev@haltian.com>
2015-08-03 18:10:00 +0300

    Remove reporters/email.js, because it is a duplicate of publishers/email.js

--- lib.in/jobs/job/reporters.js
+++ lib.in/jobs/job/reporters.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var util = require('../../misc/util.js');
-var Email = require('./reporters/email.js');
+var Email = require('./publishers/email.js');
 var FindBugs = require('./publishers/findbugs.js');
 
 uppableArrayProxy(Reporters)
--- lib.in/jobs/job/reporters/email.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var util = require('../../../misc/util.js');
-
-uppableObjectProxy(Email)
-primitive(util, Email, 'recipients')
-primitive(util, Email, 'notify-every-unstable-build')
-primitive(util, Email, 'send-to-individuals')
