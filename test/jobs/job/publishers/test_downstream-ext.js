d43b26a75c64250b5bc57b02a3bf53fb05308bb0
Roman Saveljev <roman.saveljev@haltian.com>
2015-08-03 14:13:38 +0300

    Test downstream-ext publisher

---
+++ test/jobs/job/publishers/test_downstream-ext.js
@@ -0,0 +1,13 @@
+var should = require('should');
+var DownstreamExt = require('../../../../lib/jobs/job/publishers/downstream-ext.js');
+var testlib = require('../../../testlib.js');
+
+describe('DownstreamExt', function() {
+  describe('constructor', testlib.describeProxyConstructor(DownstreamExt));
+  describe('object', testlib.describeProxyUppableAndable(DownstreamExt));
+  describe('properties', testlib.describeProxyPrimitiveArrayProperty(DownstreamExt, 'projects'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(DownstreamExt, 'comparison'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(DownstreamExt, 'criteria'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(DownstreamExt, 'only-on-scm-change'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(DownstreamExt, 'only-on-local-scm-change'));
+});
