70bccb8c55d4f9dfd08cf2b8189937749f312f1c
Roman Saveljev <roman.saveljev@haltian.com>
2015-08-04 00:21:00 +0300

    Test script trigger

---
+++ test/jobs/job/triggers/test_script.js
@@ -0,0 +1,13 @@
+var should = require('should');
+var Script = require('../../../../lib/jobs/job/triggers/script.js');
+var testlib = require('../../../testlib.js');
+
+describe('Script', function() {
+  describe('constructor', testlib.describeProxyConstructor(Script));
+  describe('object', testlib.describeProxyUppableAndable(Script));
+  describe('properties', testlib.describeProxyPrimitiveProperty(Script, 'label'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(Script, 'script'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(Script, 'cron'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(Script, 'enable-concurrent'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(Script, 'exit-code'));
+});
