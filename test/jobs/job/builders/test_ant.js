b8a040eb8fd93533d833a31936b9abeb9e2fcb46
Roman Saveljev <roman.saveljev@haltian.com>
2015-09-24 16:32:08 +0300

    Need tests for builders/ant/properties #16

--- test/jobs/job/builders/test_ant.js
+++ test/jobs/job/builders/test_ant.js
@@ -1,5 +1,6 @@
 var should = require('should');
 var Ant = require('../../../../lib/jobs/job/builders/ant.js');
+var Properties = require('../../../../lib/jobs/job/builders/ant/properties.js');
 var testlib = require('../../../testlib.js');
 
 describe('Ant', function() {
@@ -7,8 +8,7 @@ describe('Ant', function() {
   describe('object', testlib.describeProxyUppableAndable(Ant));
   describe('properties', testlib.describeProxyPrimitiveProperty(Ant, 'targets'));
   describe('properties', testlib.describeProxyPrimitiveProperty(Ant, 'buildfile'));
-  // TODO: not implemented
-  //describe('properties', testlib.describeProxyPrimitiveProperty(Ant, 'properties'));
+  describe('properties', testlib.describeProxyObjectProperty(Ant, 'properties', Properties));
   describe('properties', testlib.describeProxyPrimitiveProperty(Ant, 'ant-name'));
   describe('properties', testlib.describeProxyPrimitiveProperty(Ant, 'java-opts'));
 });
