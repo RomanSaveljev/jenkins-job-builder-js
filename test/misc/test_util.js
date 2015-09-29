4a9b3384192b0f4fc76741cc2da64756da500eae
Roman Saveljev <roman.saveljev@haltian.com>
2015-09-29 15:47:47 +0300

    Remove unused functions from lib/misc/util.js #39

--- test/misc/test_util.js
+++ test/misc/test_util.js
@@ -3,57 +3,43 @@ var util = require('../../lib/misc/util.js');
 var PrimitiveArrayProxy = require('../../lib/misc/primitive-array-proxy.js');
 var ObjectArrayProxy = require('../../lib/misc/object-array-proxy.js');
 
-describe('Primitive accessor generator', function() {
-  describe('simply', function() {
-    it('returns a function', function() {
-      should(util.generatePrimitiveAccessor('a', 'b')).be.Function();
-    });
+describe('__Primitive accessor', function() {
+  var obj = {obj: {inner: 5}};
+  it('sets inner field value', function() {
+    util.primitiveAccessor.apply(obj, ['inner', 'text']);
+    should(obj.obj.inner).be.exactly('text');
   });
-  describe('Generated function', function() {
-    var accessor = util.generatePrimitiveAccessor('outer', 'inner');
-    var obj = {outer: {inner: 5}};
-    it('sets inner field value', function() {
-      accessor.apply(obj, ['text']);
-      should(obj.outer.inner).be.exactly('text');
-    });
-    it('setter returns the object', function() {
-      should(accessor.apply(obj, [null])).be.exactly(obj);
-    });
+  it('setter returns the object', function() {
+    should(util.primitiveAccessor.apply(obj, ['inner', null])).be.exactly(obj);
   });
 });
 
-describe('Object accessor generator', function() {
-  describe('simply', function() {
-    it('returns a function', function() {
-      should(util.generateObjectAccessor('a', 'b', function() {return this})).be.Function();
-    });
-  });
-  describe('Generated function', function() {
+describe('__Object accessor', function() {
+  describe('', function() {
     var constructor = function(upper, shared) {
       this.upper = upper;
       this.shared = shared;
       return this;
     };
-    var accessor = util.generateObjectAccessor('outer', 'inner', constructor);
     it('sets inner field value to empty object', function() {
-      var obj = {outer: {}};
-      accessor.apply(obj);
-      should(obj.outer.inner).be.Object();
-      should(obj.outer.inner).be.empty();
+      var obj = {obj: {}};
+      util.objectAccessor.apply(obj, ['inner', constructor]);
+      should(obj.obj.inner).be.Object();
+      should(obj.obj.inner).be.empty();
     });
     it('drops existing inner object', function() {
-      var obj = {outer: {key: 3}};
-      accessor.apply(obj);
-      should(obj.outer.inner).be.Object();
-      should(obj.outer.inner).be.empty();
+      var obj = {obj: {inner: 3}};
+      util.objectAccessor.apply(obj, ['inner', constructor]);
+      should(obj.obj.inner).be.Object();
+      should(obj.obj.inner).be.empty();
     });
     it('passes correct upper to constructor', function() {
-      var obj = {outer: {key: 3}};
-      var proxy = accessor.apply(obj);
+      var obj = {obj: {inner: 3}};
+      var proxy = util.objectAccessor.apply(obj, ['inner', constructor]);
       should(proxy.upper).be.exactly(obj);
     });
   });
-  describe('Generated function', function() {
+  describe('', function() {
     var constructor = function(upper, shared) {
       this.upper = upper;
       this.shared = shared;
@@ -61,121 +47,92 @@ describe('Object accessor generator', function() {
       this.shared.property = true;
       return this;
     };
-    var accessor = util.generateObjectAccessor('outer', 'inner', constructor);
     it('passes correct shared to constructor', function() {
-      var obj = {outer: {key: true}};
-      var proxy = accessor.apply(obj);
+      var obj = {obj: {inner: true}};
+      var proxy = util.objectAccessor.apply(obj, ['inner', constructor]);
       should(proxy.shared).have.property('property');
     });
   });
 });
 
-describe('Primitive array accessor generator', function() {
-  describe('Basic sanity', function() {
-    it('returns function', function() {
-      should(util.generatePrimitiveArrayAccessor('a', 'b')).be.Function();
-    });
+describe('__Primitive array accessor', function() {
+  var accessor = util.primitiveArrayAccessor;
+  it('returns PrimitiveArrayProxy object', function() {
+    var obj = {obj: {inner: []}};
+    should(accessor.apply(obj, ['inner'])).be.instanceof(PrimitiveArrayProxy);
   });
-  describe('Generated function', function() {
-    var accessor = util.generatePrimitiveArrayAccessor('outer', 'inner');
-    it('returns PrimitiveArrayProxy object', function() {
-      var obj = {outer: {inner: []}};
-      should(accessor.apply(obj)).be.instanceof(PrimitiveArrayProxy);
-    });
-    it('creates array as needed', function() {
-      var obj = {outer: {}};
-      accessor.apply(obj);
-      should(obj.outer.inner).be.Array();
-    });
-    it('always drops existing array', function() {
-      var obj = {outer: {inner: ['first']}};
-      accessor.apply(obj);
-      should(obj.outer.inner).be.empty();
-    });
+  it('creates array as needed', function() {
+    var obj = {obj: {}};
+    accessor.apply(obj, ['inner']);
+    should(obj.obj.inner).be.Array();
+  });
+  it('always drops existing array', function() {
+    var obj = {obj: {inner: ['first']}};
+    accessor.apply(obj, ['inner']);
+    should(obj.obj.inner).be.empty();
   });
 });
 
-describe('Object array accessor generator', function() {
+describe('__Object array accessor', function() {
   var constructor = function() {
     return this;
   };
-  describe('Basic sanity', function() {
-    it('returns function', function() {
-      should(util.generateObjectArrayAccessor('a', 'b', constructor)).be.Function();
-    });
+  var accessor = util.objectArrayAccessor;
+  it('returns ObjectArrayProxy object', function() {
+    var obj = {obj: {inner: []}};
+    should(accessor.apply(obj, ['inner'])).be.instanceof(ObjectArrayProxy);
   });
-  describe('Generated function', function() {
-    var accessor = util.generateObjectArrayAccessor('outer', 'inner', constructor);
-    it('returns ObjectArrayProxy object', function() {
-      var obj = {outer: {inner: []}};
-      should(accessor.apply(obj)).be.instanceof(ObjectArrayProxy);
-    });
-    it('creates array as needed', function() {
-      var obj = {outer: {}};
-      accessor.apply(obj);
-      should(obj.outer.inner).be.Array();
-    });
-    it('always drops existing array', function() {
-      var obj = {outer: {inner: ['first']}};
-      accessor.apply(obj);
-      should(obj.outer.inner).be.empty();
-    });
+  it('creates array as needed', function() {
+    var obj = {obj: {}};
+    accessor.apply(obj, ['inner']);
+    should(obj.obj.inner).be.Array();
+  });
+  it('always drops existing array', function() {
+    var obj = {obj: {inner: ['first']}};
+    accessor.apply(obj, ['inner']);
+    should(obj.obj.inner).be.empty();
   });
 });
 
-
-describe('Keyed object element accessor generator', function() {
-  describe('Basic sanity', function() {
-    it('returns function', function() {
-      var constructor = function() {
-        return this;
-      };
-      should(util.generateKeyedObjectElementAccessor('array', 'key', constructor)).be.Function();
+describe('Keyed object element accessor', function() {
+  var flagger = util.keyedObjectElementAccessor;
+  describe('getter', function() {
+    var constructor = function() {
+      return this;
+    }
+    it('adds member', function() {
+      var obj = {array: []};
+      flagger.apply(obj, ['key', constructor]);
+      should(obj.array).containEql({key: {}});
     });
   });
-  describe('Generated function', function() {
-    describe('getter', function() {
-      var constructor = function() {
+  describe('proxy constructor', function() {
+    it('is called', function() {
+      var obj = {array: []};
+      var executed = false;
+      var constructor = function(parent) {
+        executed = true;
         return this;
-      }
-      var flagger = util.generateKeyedObjectElementAccessor('array', 'key', constructor);
-      it('adds member', function() {
-        var obj = {array: []};
-        flagger.apply(obj);
-        should(obj.array).containEql({key: {}});
-      });
+      };
+      flagger.apply(obj, ['flag', constructor]);
+      should(executed).be.exactly(true);
     });
-    describe('proxy constructor', function() {
-      it('is called', function() {
-        var obj = {array: []};
-        var executed = false;
-        var constructor = function(parent) {
-          executed = true;
-          return this;
-        };
-        var flagger = util.generateKeyedObjectElementAccessor('array', 'flag', constructor);
-        flagger.apply(obj);
-        should(executed).be.exactly(true);
-      });
-      it('receives parent object', function() {
-        var obj = {array: []};
-        var constructor = function(parent) {
-          should(parent).be.exactly(obj);
-          return this;
-        };
-        var flagger = util.generateKeyedObjectElementAccessor('array', 'flag', constructor);
-        flagger.apply(obj);
-      });
-      it('receives inner empty object', function() {
-        var obj = {array: []};
-        var constructor = function(parent, inner) {
-          should(inner).be.Object();
-          should(inner).be.empty();
-          return this;
-        };
-        var flagger = util.generateKeyedObjectElementAccessor('array', 'flag', constructor);
-        flagger.apply(obj);
-      });
+    it('receives parent object', function() {
+      var obj = {array: []};
+      var constructor = function(parent) {
+        should(parent).be.exactly(obj);
+        return this;
+      };
+      flagger.apply(obj, ['flag', constructor]);
+    });
+    it('receives inner empty object', function() {
+      var obj = {array: []};
+      var constructor = function(parent, inner) {
+        should(inner).be.Object();
+        should(inner).be.empty();
+        return this;
+      };
+      flagger.apply(obj, ['flag', constructor]);
     });
   });
 });
