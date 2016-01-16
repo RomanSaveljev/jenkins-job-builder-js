cc6de98a43a8004910122bacf4d542d06b1d4d4b
Roman Saveljev <roman.saveljev@haltian.com>
2016-01-16 16:12:50 +0200

    Rename publishers/downstream-ext:comparison to condition #64

--- lib.in/jobs/job/publishers/downstream-ext.js
+++ lib.in/jobs/job/publishers/downstream-ext.js
@@ -4,7 +4,7 @@ var util = require('../../../misc/util.js');
 
 uppableObjectProxy(DownstreamExt)
 primitiveArray(util, DownstreamExt, 'projects')
-primitive(util, DownstreamExt, 'comparison')
+primitive(util, DownstreamExt, 'condition')
 primitive(util, DownstreamExt, 'criteria')
 primitive(util, DownstreamExt, 'only-on-scm-change')
 primitive(util, DownstreamExt, 'only-on-local-scm-change')
--- test/jobs/job/publishers/test_downstream-ext.js
+++ test/jobs/job/publishers/test_downstream-ext.js
@@ -6,7 +6,7 @@ describe('DownstreamExt', function() {
   describe('constructor', testlib.describeProxyConstructor(DownstreamExt));
   describe('object', testlib.describeProxyUppableAndable(DownstreamExt));
   describe('properties', testlib.describeProxyPrimitiveArrayProperty(DownstreamExt, 'projects'));
-  describe('properties', testlib.describeProxyPrimitiveProperty(DownstreamExt, 'comparison'));
+  describe('properties', testlib.describeProxyPrimitiveProperty(DownstreamExt, 'condition'));
   describe('properties', testlib.describeProxyPrimitiveProperty(DownstreamExt, 'criteria'));
   describe('properties', testlib.describeProxyPrimitiveProperty(DownstreamExt, 'only-on-scm-change'));
   describe('properties', testlib.describeProxyPrimitiveProperty(DownstreamExt, 'only-on-local-scm-change'));
